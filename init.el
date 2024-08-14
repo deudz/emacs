6c6cb5554c2162ef7f886625b446eaeaf066ce6f
Danilo Soares <deudzdev@gmail.com>
2024-08-14 16:18:27 -0300

    Várias coisas mudadas... denovo...
    Adição(+)
    Remoção(-)
    Mudança(*)
    
    + org-bullets: marcadores bonitos no org-mode
    + helpful: documentação mais útil e bonita
    + swiper: pesquisa melhor
    + diredfl: dired colorido
    + bootstrap script: execute este script para instalar as coisas
    - bootstrap.el: removido para o uso do script
    * tema: doom-old-hope
    * atalhos:

--- init.el
+++ init.el
@@ -7,9 +7,21 @@
 (require 'org)
 (require 'ob-tangle)
 
-(load-file (concat user-emacs-directory "bootstrap.el"))
+(defun load-init () (load (expand-file-name
+			   "straight/repos/straight.el/bootstrap.el"
+			   user-emacs-directory)
+			  nil 'nomessage)
+       (org-babel-load-file (expand-file-name "config.org" user-emacs-directory)))
 
-(org-babel-load-file (concat user-emacs-directory "config.org"))
+(if (file-exists-p (expand-file-name
+		    "straight/repos/straight.el/bootstrap.el"
+		    user-emacs-directory))
+    (load-init)
+  (warn
+   (concat
+    "Por favor execute " (abbreviate-file-name
+		   (concat user-emacs-directory "bootstrap"))
+    " como um USUÁRIO NORMAL para instalar estas configurações!")))
 
 (setq gc-cons-threshold most-positive-fixnum)
 
